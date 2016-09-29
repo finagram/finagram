650abef6b37106e0d9123bd0a530e1c6fdec2982
vladimir <vladimir.p@livetex.ru>
2016-09-29 11:27:21 +0300

    remove text from PhotoMessage

--- finagram/src/main/scala/ru/finagram/api/Message.scala
+++ finagram/src/main/scala/ru/finagram/api/Message.scala
@@ -29,7 +29,6 @@ case class TextMessage(messageId: Long, from: Option[User], date: Long, chat: Ch
  * @param date    	Date the message was sent in Unix time.
  * @param chat 	    Conversation the message belongs to.
  * @param sticker   Information about the sticker.
- * @param text      The actual UTF-8 text of the message, 0-4096 characters.
  */
 case class StickerMessage(messageId: Long, from: Option[User], date: Long, chat: Chat, sticker: Sticker) extends Message
 
@@ -37,7 +36,7 @@ case class DocumentMessage(messageId: Long, from: Option[User], date: Long, chat
 
 case class LocationMessage(messageId: Long, from: Option[User], date: Long, chat: Chat, location: Location) extends Message
 
-case class PhotoMessage(messageId: Long, from: Option[User], date: Long, chat: Chat, photo: List[PhotoSize], text: Option[String]) extends Message
+case class PhotoMessage(messageId: Long, from: Option[User], date: Long, chat: Chat, photo: List[PhotoSize]) extends Message
 
 case class VideoMessage(messageId: Long, from: Option[User], date: Long, chat: Chat, video: Video) extends Message
 
